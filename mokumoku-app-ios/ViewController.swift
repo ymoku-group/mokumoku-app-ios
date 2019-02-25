b4b996cfdfefe47eb1d5ade243ac881424859b23
aseo <aseo@yahoo-corp.jp>
2019-02-25 18:02:45 +0900

    init ios project.

---
+++ mokumoku-app-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mokumoku-app-ios
+//
+//  Created by aseo on 2019/02/25.
+//  Copyright © 2019年 YMoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ mokumoku-app-iosTests/mokumoku_app_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  mokumoku_app_iosTests.swift
+//  mokumoku-app-iosTests
+//
+//  Created by aseo on 2019/02/25.
+//  Copyright © 2019年 YMoku. All rights reserved.
+//
+
+import XCTest
+@testable import mokumoku_app_ios
+
+class mokumoku_app_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
