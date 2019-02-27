04008e9a1d6c41e725f0fcafd40650844e91a2fc
aseo <aseo@yahoo-corp.jp>
2019-02-27 22:26:55 +0900

    feat: Mastodonモジュールの新規追加

---
+++ Mastodon/Mastodon.h
@@ -0,0 +1,19 @@
+//
+//  Mastodon.h
+//  Mastodon
+//
+//  Created by aseo on 2019/02/25.
+//  Copyright © 2019年 YMoku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mastodon.
+FOUNDATION_EXPORT double MastodonVersionNumber;
+
+//! Project version string for Mastodon.
+FOUNDATION_EXPORT const unsigned char MastodonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mastodon/PublicHeader.h>
+
+
